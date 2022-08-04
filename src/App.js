fb23768fdfe40b41feb93661642a54b350166dd7
akinus <42932934+akimo10@users.noreply.github.com>
2022-08-05 00:56:46 +0900

    ネストしたページ遷移ルーティングを実装

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
 
 import { Home } from "./Home";
 import { Page1 } from "./Page1";
+import { Page1DetailA } from "./Page1DetailA";
+import { Page1DetailB } from "./Page1DetailB";
 import { Page2 } from "./Page2";
 import "./styles.css";
 
@@ -20,10 +22,18 @@ export default function App() {
         <Route exact path="/">
           <Home />
         </Route>
-        <Route exact path="/page1">
-          <Page1 />
-        </Route>
-        <Route exact path="/page2">
+        <Route
+          path="/page1"
+          render={({ match: { url } }) => (
+            <Switch>
+              {console.log(url)}
+              <Route exact path={url} render={() => <Page1 />} />
+              <Route path={`${url}/detailA`} render={() => <Page1DetailA />} />
+              <Route path={`${url}/detailB`} render={() => <Page1DetailB />} />
+            </Switch>
+          )}
+        />
+        <Route path="/page2">
           <Page2 />
         </Route>
       </Switch>
